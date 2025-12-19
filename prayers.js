042e243cf6e09b5a2ee9baf66726f0bf2864867b
Georges Kmeid <georgesk117@gmail.com>
2025-12-19 08:21:48 +0700

    update prayers.js

--- prayers.js
+++ prayers.js
@@ -1,7 +1,7 @@
 export const PRAYERS = {
-  apostlesCreed: 'I believe in God...',
-  ourFather: 'Our Father...',
-  hailMary: 'Hail Mary...',
-  gloryBe: 'Glory be...',
-  fatimaPrayer: 'O my Jesus...',
+  apostlesCreed: 'I believe in God, the Father Almighty, Creator of Heaven and earth; and in Jesus Christ, His only Son, Our Lord, Who was conceived by the Holy Ghost, born of the Virgin Mary, suffered under Pontius Pilate, was crucified; died, and was buried. He descended into Hell; the third day He arose again from the dead; He ascended into Heaven, sitteth at the right hand of God, the Father Almighty; from thence He shall come to judge the living and the dead. I believe in the Holy Spirit, the holy Catholic Church, the communion of saints, the forgiveness of sins, the resurrection of the body, and the life everlasting. Amen.',
+  ourFather: 'Our Father, Who art in heaven, hallowed be Thy name; Thy kingdom come; Thy will be done on earth as it is in heaven. Give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; and lead us not into temptation, but deliver us from evil, Amen.',
+  hailMary: 'Hail Mary, full of grace. The Lord is with thee. Blessed art thou amongst women, and blessed is the fruit of thy womb, Jesus. Holy Mary, Mother of God, pray for us sinners, now and at the hour of our death, Amen.',
+  gloryBe: 'Glory be to the Father, and to the Son, and to the Holy Spirit. As it was in the beginning, is now, and ever shall be, world without end. Amen.',
+  fatimaPrayer: 'O my Jesus, forgive us our sins, save us from the fires of hell, lead all souls to Heaven, especially those in most need of Thy mercy.',
 }
