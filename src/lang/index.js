c2f20ccc548a350549de043def1a6dea76675994
Georges Kmeid <georgesk117@gmail.com>
2025-12-26 20:25:20 +0700

    add latin language file

--- src/lang/index.js
+++ src/lang/index.js
@@ -2,6 +2,8 @@ export async function loadLang (code) {
   switch (code) {
     case 'en':
       return import('./en.js').then(m => m.default)
+    case 'la':
+      return import('./la.js').then(m => m.default)
     default:
       throw new Error(`Unsupported language: ${code}`)
   }
---
+++ src/lang/la.js
@@ -0,0 +1,35 @@
+export default {
+  prayers: {
+    apostlesCreed: 'Credo in Deum, Patrem omnipotentem, creatorem caeli et terrae. Et in Iesum Christum, Filium eius unicum, Dominum nostrum, qui conceptus est de Spiritu Sancto, natus ex Maria Virgine, passus sub Pontio Pilato, crucifixus, mortuus et sepultus. Descendit ad inferos, tertia die resurrexit a mortuis. Ascendit ad caelos, sedet ad dextram Dei Patris omnipotentis. Inde venturus est iudicare vivos et mortuos. Credo in Spiritum Sanctum, sanctam Ecclesiam catholicam, sanctorum communionem, remissionem peccatorum, carnis resurrectionem, vitam aeternam. Amen.',
+    ourFather: 'Pater noster, qui es in caelis: Sanctificetur nomen tuum: Adveniat regnum tuum: Fiat voluntas tua, sicut in caelo, et in terra. Panem nostrum quotidianum da nobis hodie: Et dimitte nobis debita nostra, sicut et nos dimittimus debitoribus nostris. Et ne nos inducas in tentationem, sed libera nos a malo. Amen.',
+    hailMary: 'Ave Maria, gratia plena: Dominus tecum, benedicta tu in mulieribus, et benedictus fructus ventris tui, Iesus. Sancta Maria, Mater Dei, ora pro nobis peccatoribus, nunc et in hora mortis nostrae. Amen.',
+    gloryBe: 'Gloria Patri, et Filio, et Spiritui Sancto. Sicut erat in principio, et nunc, et semper, et in saecula saeculorum. Amen.',
+    fatimaPrayer: 'Domine Iesu, dimitte nobis debita nostra, salva nos ab igne inferni, perduc in caelum omnes animas, praesertim eas quae misericordiae tuae maximae indigent.',
+  },
+
+  mysteries: {
+    annunciation: 'Annuntiatio',
+    visitation: 'Visitatio',
+    nativity: 'Nativitas',
+    presentation: 'Presentatio',
+    findingInTemple: 'Inventio in Templo',
+
+    baptism: 'Baptismus Christi',
+    weddingAtCana: 'Nuptiae in Canense',
+    proclaimingKingdom: 'Annuntiatio Regni Dei',
+    transfiguration: 'Transfiguratio Domini',
+    institutionEucharist: 'Institutio Eucharistiae',
+
+    agonyInGarden: 'Cruciatus in Horto',
+    scourgingAtPillar: 'Flagellatio',
+    crowningWithThorns: 'Coronatio cum Spinis',
+    carryingCross: 'Bajulatio Crucis',
+    crucifixion: 'Crucifixio',
+
+    resurrection: 'Resurrectio',
+    ascension: 'Ascensio',
+    descentHolySpirit: 'Descensus Spiritus Sancti',
+    assumption: 'Assumptio',
+    coronation: 'Coronatio'
+  },
+}
